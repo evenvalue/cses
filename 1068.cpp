94bd8729a539be6b1d3fb8cb81da37e60b2f3837
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-03-31 01:00:07 +0530

    Add implementation problem.

---
+++ 1068.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define int64 int64_t
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int64 n;
+  cin >> n;
+  while (n != 1) {
+    cout << n << " ";
+    if (n % 2 == 0) {
+      n /= 2;
+    } else {
+      n *= 3;
+      n++;
+    }
+  }
+  cout << 1 << "\n";
+}
