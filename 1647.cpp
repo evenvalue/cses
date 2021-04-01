4b7c2c8192e9ca0465dda63d3dce32de734d051a
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-01 23:21:08 +0530

    Add segment tree problem.

---
+++ 1647.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class SegTree {
+  vector<int> t;
+
+  static int unite(const int l, const int r) {
+    return min(l, r);
+  }
+
+public:
+  explicit SegTree(const int n) : t(2 * n - 1) {}
+
+  void build(const int x, const int l, const int r, const vector<int> &a) {
+    if (l == r) {
+      t[x] = a[l];
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    build(x + 1, l, mid, a);
+    build(y, mid + 1, r, a);
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  int query(const int x, const int l, const int r, const int ql, const int qr) {
+    if (ql <= l and r <= qr) {
+      return t[x];
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (qr <= mid) {
+      return query(x + 1, l, mid, ql, qr);
+    } else if (mid + 1 <= ql) {
+      return query(y, mid + 1, r, ql, qr);
+    } else {
+      return unite(query(x + 1, l, mid, ql, qr), query(y, mid + 1, r, ql, qr));
+    }
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (int &x : a) {
+    cin >> x;
+  }
+  SegTree st(n);
+  st.build(0, 0, n - 1, a);
+  while (q--) {
+    int l, r;
+    cin >> l >> r;
+    cout << st.query(0, 0, n - 1, l - 1, r - 1) << "\n";
+  }
+}
