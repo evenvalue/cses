e75d9e8ade41e73dae2035f80e023596c70bc790
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-12 16:48:45 +0530

    Replace two bfs's in diameter and main with a single bfs function.

--- 1132.cpp
+++ 1132.cpp
@@ -4,28 +4,30 @@
 #include <vector>
 using namespace std;
 
-pair<int, int> diameter(const vector<vector<int>> &g) {
-  auto bfs = [&](int start) -> int {
-    const int n = g.size();
-    vector<int> d(n, 0);
-    vector<bool> visit(n, false);
-    queue<int> q;
-    q.push(start);
-    while (not q.empty()) {
-      int x = q.front();
-      q.pop();
-      visit[x] = true;
-      for (int nbr : g[x]) {
-        if (visit[nbr]) continue;
-        q.push(nbr);
-        d[nbr] = d[x] + 1;
-      }
+int bfs(const vector<vector<int>> &g, vector<int> &d, const int s) {
+  const int n = g.size();
+  vector<bool> visit(n, false);
+  d[s] = 0;
+  queue<int> q;
+  q.push(s);
+  while (not q.empty()) {
+    int x = q.front();
+    q.pop();
+    visit[x] = true;
+    for (int nbr : g[x]) {
+      if (visit[nbr]) continue;
+      q.push(nbr);
+      d[nbr] = d[x] + 1;
     }
-    int far = distance(d.begin(), max_element(d.begin(), d.end()));
-    return far;
-  };
-  int x = bfs(0);
-  int y = bfs(x);
+  }
+  int far = distance(d.begin(), max_element(d.begin(), d.end()));
+  return far;
+}
+
+pair<int, int> diameter(const vector<vector<int>> &g) {
+  vector<int> d(g.size());
+  int x = bfs(g, d, 0);
+  int y = bfs(g, d, x);
   return {x, y};
 }
 
@@ -43,28 +45,11 @@ int main() {
     g[x - 1].push_back(y - 1);
     g[y - 1].push_back(x - 1);
   }
-
-  auto bfs = [&](vector<int> &d, int start) {
-    const int n = g.size();
-    vector<bool> visit(n, false);
-    queue<int> q;
-    q.push(start);
-    while (not q.empty()) {
-      int x = q.front();
-      q.pop();
-      visit[x] = true;
-      for (int nbr : g[x]) {
-        if (visit[nbr]) continue;
-        q.push(nbr);
-        d[nbr] = d[x] + 1;
-      }
-    }
-  };
   vector<int> dx(n);
   vector<int> dy(n);
   pair<int, int> endp = diameter(g);
-  bfs(dx, endp.first);
-  bfs(dy, endp.second);
+  bfs(g, dx, endp.first);
+  bfs(g, dy, endp.second);
   for (int i = 0; i < n; i++) {
     cout << max(dx[i], dy[i]) << " ";
   }
