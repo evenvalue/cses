7c6fdd723b1c524e54153c9e8659c50fa752ec03
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-12 16:30:07 +0530

    Add segment tree on trees problem.

---
+++ 1137.cpp
@@ -0,0 +1,103 @@
+#include <functional>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+
+struct node {
+  int value;
+  int l, r;
+};
+
+class SegTree {
+  vector<int64> t;
+
+  static int64 unite(const int64 l, const int64 r) {
+    return l + r;
+  }
+
+public:
+  explicit SegTree(const int n) : t(2 * n - 1) {}
+
+  void update(const int x, const int l, const int r, const int pos, const int upd) {
+    if (l == r) {
+      t[x] = upd;
+      return;
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (pos <= mid) {
+      update(x + 1, l, mid, pos, upd);
+    } else {
+      update(y, mid + 1, r, pos, upd);
+    }
+    t[x] = unite(t[x + 1], t[y]);
+  }
+
+  int64 query(const int x, const int l, const int r, const int ql, const int qr) {
+    if (ql <= l and r <= qr) {
+      return t[x];
+    }
+    const int mid = (l + r) / 2;
+    const int y = 2 * (mid - l + 1) + x;
+    if (qr <= mid) {
+      return query(x + 1, l, mid, ql, qr);
+    } else if (mid + 1 <= ql) {
+      return query(y, mid + 1, r, ql, qr);
+    } else {
+      return unite(query(x + 1, l, mid, ql, qr), query(y, mid + 1, r, ql, qr));
+    }
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int n, q;
+  cin >> n >> q;
+  vector<node> a(n);
+  for (node &x : a) {
+    cin >> x.value;
+  }
+  vector<vector<int>> g(n);
+  for (int i = 0; i < n - 1; i++) {
+    int x, y;
+    cin >> x >> y;
+    g[x - 1].push_back(y - 1);
+    g[y - 1].push_back(x - 1);
+  }
+  vector<int> preorder;
+  function<void(int, int)> build_preorder = [&](const int x, const int p) {
+    a[x].l = preorder.size();
+    preorder.push_back(x);
+    for (const int &y : g[x]) {
+      if (y == p) continue;
+      build_preorder(y, x);
+    }
+    a[x].r = (int)preorder.size() - 1;
+  };
+  build_preorder(0, -1);
+  SegTree st(n);
+  for (const node &x : a) {
+    st.update(0, 0, n - 1, x.l, x.value);
+  }
+  while (q--) {
+    int type;
+    cin >> type;
+    if (type == 1) {
+      int vertex, upd;
+      cin >> vertex >> upd;
+      vertex--;
+      a[vertex].value = upd;
+      st.update(0, 0, n - 1, a[vertex].l, upd);
+    } else {
+      int vertex;
+      cin >> vertex;
+      vertex--;
+      cout << st.query(0, 0, n - 1, a[vertex].l, a[vertex].r) << "\n";
+    }
+  }
+}
