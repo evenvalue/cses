0f57f9c38fdc7a895305b8e201a36a058ddb895d
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-17 03:07:32 +0530

    Add space optimization dp.

--- 1638.cpp
+++ 1638.cpp
@@ -8,27 +8,27 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  const int mod = 1e9 + 7;
+  const int kMod = 1e9 + 7;
 
   int n;
   cin >> n;
-  vector<vector<int>> dp(n, vector<int>(n, 0));
-  dp[0][0] = 1;
-  for (int r = 0; r < n; r++) {
-    string row;
+  vector<vector<int>> dp(2, vector<int>(n));
+  string row;
+  cin >> row;
+  dp[0][0] = (row[0] == '.');
+  for (int c = 1; c < n; c++) {
+    dp[0][c] = (row[c] == '.') * dp[0][c - 1];
+  }
+  for (int r = 1, bit = 1; r < n; r++, bit = 1 - bit) {
     cin >> row;
-    for (int c = 0; c < n; c++) {
+    dp[bit][0] = (row[0] == '.') * dp[1 - bit][0];
+    for (int c = 1; c < n; c++) {
       if (row[c] == '.') {
-        if (0 < r) {
-          (dp[r][c] += dp[r - 1][c]) %= mod;
-        }
-        if (0 < c) {
-          (dp[r][c] += dp[r][c - 1]) %= mod;
-        }
+        dp[bit][c] = (dp[1 - bit][c] + dp[bit][c - 1]) % kMod;
       } else {
-        dp[r][c] = 0;
+        dp[bit][c] = 0;
       }
     }
   }
-  cout << dp[n - 1][n - 1] << "\n";
+  cout << dp[(n - 1) % 2][n - 1] << "\n";
 }
