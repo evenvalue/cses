2cbb85840e0ef2a9c49bd085c5dacef610fa113d
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-17 01:36:18 +0530

    Change ans -> dp.

--- 1634.cpp
+++ 1634.cpp
@@ -15,13 +15,13 @@ int main() {
   for (int &coin : coins) {
     cin >> coin;
   }
-  vector<int> ans(x + 1, kInf);
-  ans[0] = 0;
+  vector<int> dp(x + 1, kInf);
+  dp[0] = 0;
   for (int i = 0; i < x; i++) {
     for (const int coin : coins) {
       if (i + coin > x) continue;
-      ans[i + coin] = min(ans[i + coin], ans[i] + 1);
+      dp[i + coin] = min(dp[i + coin], dp[i] + 1);
     }
   }
-  cout << (ans[x] == kInf ? -1 : ans[x]) << "\n";
+  cout << (dp[x] == kInf ? -1 : dp[x]) << "\n";
 }
