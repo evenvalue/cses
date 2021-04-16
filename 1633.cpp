2488e891fd21ca6325d35943e83c13205033d6a7
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-17 01:22:26 +0530

    Use range for loop.

--- 1633.cpp
+++ 1633.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 using namespace std;
 
-#define int64 int64_t
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -12,16 +10,13 @@ int main() {
   const int mod = 1e9 + 7;
   int n;
   cin >> n;
-  vector<int64> ways(n + 6);
+  vector<int> ways(n + 6);
   ways[0] = 1;
   for (int i = 0; i < n; i++) {
-    const int me = ways[i];
-    ways[i + 1] = (ways[i + 1] + me) % mod;
-    ways[i + 2] = (ways[i + 2] + me) % mod;
-    ways[i + 3] = (ways[i + 3] + me) % mod;
-    ways[i + 4] = (ways[i + 4] + me) % mod;
-    ways[i + 5] = (ways[i + 5] + me) % mod;
-    ways[i + 6] = (ways[i + 6] + me) % mod;
+    for (const int x : {1, 2, 3, 4, 5, 6}) {
+      ways[i + x] += ways[i];
+      ways[i + x] %= mod;
+    }
   }
   cout << ways[n] << "\n";
 }
