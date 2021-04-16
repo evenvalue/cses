5015b0409acdf867e770327b74a50d9accb59654
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-17 02:22:34 +0530

    Perform space optimisation dp.
    Old: O(n * req) space, time: 0.57s
    New: O(2 * req) space, time: 0.33s

--- 1636.cpp
+++ 1636.cpp
@@ -3,14 +3,12 @@
 #include <vector>
 using namespace std;
 
-#define int64 int64_t
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  const int mod = 1e9 + 7;
+  const int kMod = 1e9 + 7;
 
   int n, req;
   cin >> n >> req;
@@ -18,20 +16,19 @@ int main() {
   for (int &coin : coins) {
     cin >> coin;
   }
-  vector<vector<int>> dp(n, vector<int>(req + 1));
+  vector<vector<int>> dp(2, vector<int>(req + 1));
   for (int x = 0; x <= req; x++) {
     dp[0][x] = (x % coins[0] == 0);
   }
-  for (int c = 0; c < n; c++) {
-    dp[c][0] = 1;
-  }
-  for (int c = 1; c < n; c++) {
+  for (int c = 1, bit = 1; c < n; c++, bit = 1 - bit) {
+    fill(dp[bit].begin(), dp[bit].end(), 0);
+    dp[bit][0] = 1;
     for (int x = 1; x <= req; x++) {
-      dp[c][x] = dp[c - 1][x];
+      dp[bit][x] = dp[1 - bit][x];
       if (x < coins[c]) continue;
-      dp[c][x] += dp[c][x - coins[c]];
-      dp[c][x] %= mod;
+      dp[bit][x] += dp[bit][x - coins[c]];
+      dp[bit][x] %= kMod;
     }
   }
-  cout << dp[n - 1][req] << "\n";
+  cout << dp[(n - 1) % 2][req] << "\n";
 }
