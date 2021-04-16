6177311366e27aff330fa897a95490c605038486
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-17 01:34:05 +0530

    Use range for loop.

--- 1634.cpp
+++ 1634.cpp
@@ -7,20 +7,21 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  const int inf = 1e9 + 10;
+  const int kInf = 1e9 + 10;
+
   int n, x;
   cin >> n >> x;
   vector<int> coins(n);
   for (int &coin : coins) {
     cin >> coin;
   }
-  vector<int> ans(2e6 + 5, inf);
+  vector<int> ans(x + 1, kInf);
   ans[0] = 0;
   for (int i = 0; i < x; i++) {
-    for (int j = 0; j < n; j++) {
-      int &that = ans[i + coins[j]];
-      that = min(that, ans[i] + 1);
+    for (const int coin : coins) {
+      if (i + coin > x) continue;
+      ans[i + coin] = min(ans[i + coin], ans[i] + 1);
     }
   }
-  cout << (ans[x] == inf ? -1 : ans[x]) << "\n";
+  cout << (ans[x] == kInf ? -1 : ans[x]) << "\n";
 }
