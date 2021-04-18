43172155402d960f469dec10e748e2cbaa010c1c
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-04-19 01:19:19 +0530

    Create separate for loop for base case.
    Also rename dp -> cuts.

--- 1744.cpp
+++ 1744.cpp
@@ -9,18 +9,19 @@ int main() {
 
   int n, m;
   cin >> n >> m;
-  vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+  vector<vector<int>> cuts(n + 1, vector<int>(m + 1, 1e9 + 10));
+  for (int side = 0; side <= min(n, m); side++) {
+    cuts[side][side] = 0;
+  }
   for (int r = 0; r <= n; r++) {
     for (int c = 0; c <= m; c++) {
-      if (r == c) continue;
-      dp[r][c] = 1e9 + 10;
       for (int vcut = 0; vcut < c; vcut++) {
-        dp[r][c] = min(dp[r][c], dp[r][vcut] + dp[r][c - vcut] + 1);
+        cuts[r][c] = min(cuts[r][c], cuts[r][vcut] + cuts[r][c - vcut] + 1);
       }
       for (int hcut = 0; hcut < r; hcut++) {
-        dp[r][c] = min(dp[r][c], dp[hcut][c] + dp[r - hcut][c] + 1);
+        cuts[r][c] = min(cuts[r][c], cuts[hcut][c] + cuts[r - hcut][c] + 1);
       }
     }
   }
-  cout << dp[n][m] << "\n";
+  cout << cuts[n][m] << "\n";
 }
