625caa77f8abde8528bb6145dc34a9192e4434cf
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-04 11:56:10 +0530

    Make separate function for dijkstra.

--- 1671.cpp
+++ 1671.cpp
@@ -1,41 +1,63 @@
+#include <algorithm>
+#include <array>
 #include <iostream>
-#include <vector>
+#include <numeric>
 #include <set>
+#include <string>
+#include <vector>
 using namespace std;
 
 #define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
 
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+const int64 kInf = 1e15 + 10;
+const int kMod = 1e9 + 7;
 
+pair<vector<int64>, vector<int>> dijkstra(const vector<vector<pair<int, int>>> &g, const int s) {
+  const int n = g.size();
+  vector<int64> d(n, kInf);
+  vector<int> p(n, -1);
+  d[s] = 0;
+  set<pair<int64, int>> q;
+  q.insert({0, s});
+  while (not q.empty()) {
+    const int x = q.begin()->second;
+    q.erase(q.begin());
+    for (const auto [y, w] : g[x]) {
+      if (d[y] < d[x] + w) continue;
+      q.erase({d[y], y});
+      d[y] = d[x] + w;
+      q.insert({d[y], y});
+      p[y] = x;
+    }
+  }
+  return make_pair(d, p);
+}
+
+void solve() {
   int n, m;
   cin >> n >> m;
   vector<vector<pair<int, int>>> g(n);
   for (int i = 0; i < m; i++) {
     int x, y, w;
     cin >> x >> y >> w;
-    g[x - 1].push_back({w, y - 1});
+    g[x - 1].push_back({y - 1, w});
   }
-  vector<int64> dist(n, 1e15);
-  dist[0] = 0;
-  set<pair<int64, int>> q;
-  q.insert({0, 0});
-  while (not q.empty()) {
-    const int x = q.begin()->second;
-    q.erase(q.begin());
-    for (const pair<int, int> nbr : g[x]) {
-      const int w = nbr.first;
-      const int y = nbr.second;
-      if (dist[y] > dist[x] + w) {
-        q.erase({dist[y], y});
-        dist[y] = dist[x] + w;
-        q.insert({dist[y], y});
-      }
-    }
+  const auto d = dijkstra(g, 0).first;
+  for (const int64 x : d) {
+    cout << x << ' ';
   }
-  for (const int64 d : dist) {
-    cout << d << " ";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
   }
 }
