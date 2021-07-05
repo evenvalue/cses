92162352970d4794b72db180fec0cc5681d98753
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-05 11:52:18 +0530

    Improvise dijkstra function + add solve().

--- 1195.cpp
+++ 1195.cpp
@@ -1,32 +1,60 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
+#include <numeric>
 #include <set>
+#include <string>
 #include <vector>
 using namespace std;
 
 #define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
 
-const int64 inf = 1e15;
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
 
-void dijkstra(const vector<vector<pair<int, int>>> &g, const int s, vector<int64> &d) {
+pair<vector<int64>, vector<int>> dijkstra(const vector<vector<pair<int, int>>> &g, const int s) {
   const int n = g.size();
-  d.assign(n, inf);
+  vector<int64> d(n, kInfL);
+  vector<int> p(n, -1);
   d[s] = 0;
   set<pair<int64, int>> q;
   q.insert({0, s});
   while (not q.empty()) {
     const int x = q.begin()->second;
     q.erase(q.begin());
-    for (const pair<int, int> nbr : g[x]) {
-      const int y = nbr.second;
-      const int w = nbr.first;
-      if (d[x] + w < d[y]) {
-        q.erase({d[y], y});
-        d[y] = d[x] + w;
-        q.insert({d[y], y});
-      }
+    for (const auto [y, w] : g[x]) {
+      if (d[y] < d[x] + w) continue;
+      q.erase({d[y], y});
+      d[y] = d[x] + w;
+      q.insert({d[y], y});
+      p[y] = x;
     }
   }
+  return make_pair(d, p);
+}
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<pair<int, int>>> g(n), gr(n);
+  for (int i = 0; i < m; i++) {
+    int x, y, w;
+    cin >> x >> y >> w;
+    x--, y--;
+    g[x].push_back({y, w});
+    gr[y].push_back({x, w});
+  }
+  const vector<int64> ds = dijkstra(g, 0).first;
+  const vector<int64> dt = dijkstra(gr, n - 1).first;
+  int64 ans = kInfL;
+  for (int x = 0; x < n; x++) {
+    for (const auto [y, w] : g[x]) {
+      ans = min(ans, ds[x] + (w / 2) + dt[y]);
+    }
+  }
+  cout << ans;
 }
 
 int main() {
@@ -34,30 +62,10 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  struct edge {
-    int x;
-    int y;
-    int w;
-  };
-
-  int n, m;
-  cin >> n >> m;
-  vector<vector<pair<int, int>>> g(n), gr(n);
-  vector<edge> edges(m);
-  for (edge &e : edges) {
-    cin >> e.x >> e.y >> e.w;
-    e.x--;
-    e.y--;
-    g[e.x].push_back({e.w, e.y});
-    gr[e.y].push_back({e.w, e.x});
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
   }
-  vector<int64> ds, dt;
-  dijkstra(g, 0, ds);
-  dijkstra(gr, n - 1, dt);
-  int64 ans = inf;
-  for (const edge &e : edges) {
-    if (ds[e.x] == inf or dt[e.y] == inf) continue;
-    ans = min(ans, ds[e.x] + dt[e.y] + (e.w / 2));
-  }
-  cout << ans << "\n";
 }
