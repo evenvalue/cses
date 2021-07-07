4dd97c95eaa8e8f0becb7cbc3c3f9d5b2eed08b6
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-07 13:53:31 +0530

    Add math problem.
    Two Knights : https://cses.fi/problemset/task/1072

---
+++ 1072.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  for (int k = 1; k <= n; k++) {
+    const int64 totalWays = (k * k) * 1LL * (k * k - 1) / 2;
+    const int64 attackWays = 4 * (k - 1) * (k - 2);
+    cout << totalWays - attackWays << '\n';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+  }
+}
