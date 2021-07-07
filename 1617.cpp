fe9eb8d6785ba43722a9fc4fe162f7440ec46de6
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-08 02:05:46 +0530

    Add implementation problem.
    Bit Strings : https://cses.fi/problemset/task/1617

---
+++ 1617.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int n;
+  cin >> n;
+  int ans = 1;
+  for (int i = 0; i < n; i++) {
+    ans += ans;
+    ans -= (ans > kMod ? kMod : 0);
+  }
+  cout << ans;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  //cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
