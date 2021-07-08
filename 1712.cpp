7242721e5f4add57ba93a2e0e8425640dee1fdb6
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-08 11:52:53 +0530

    Add math problem.
    Exponentiation II : https://cses.fi/problemset/task/1712

---
+++ 1712.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+int bin_pow(int64 a, int b, const int mod) {
+  int64 res = 1;
+  while (b) {
+    if (b & 1) res = (res * a) % mod;
+    a = (a * a) % mod;
+    b >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << bin_pow(a, bin_pow(b, c, kMod - 1), kMod);
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
