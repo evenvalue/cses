ce0cb388b312f8cd3677ecad232b8fdf046b3b05
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2021-07-08 11:19:56 +0530

    Add math problem.
    Coin Piles : https://cses.fi/problemset/task/1754

---
+++ 1754.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <array>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define int64 int64_t
+#define debug(x) cerr << #x << ": " << x << '\n'
+
+const int64 kInfL = 1e15 + 10;
+const int kInf = 1e9 + 10;
+const int kMod = 1e9 + 7;
+
+void solve() {
+  int a, b;
+  cin >> a >> b;
+  if (2 * a >= b and 2 * b >= a and (2 * a - b) % 3 == 0 and (2 * b - a) % 3 == 0) {
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+    cout << '\n';
+  }
+}
