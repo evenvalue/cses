13cc9dbfa44c7f3646f5c52e5ad9e6ed81145049
Naveen Kulkarni <naveenpkulkarni07@gmail.com>
2024-03-05 17:01:37 +0530

    Add pascals triangle problem.
    Xor Pyramid: https://cses.fi/problemset/task/2419/

---
+++ 2419.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef evenvalue
+  #include "debug.h"
+#else
+  #define debug(...)
+#endif
+
+using int64 = long long;
+using ld = long double;
+
+template<typename T>
+using min_heap = priority_queue<T, vector<T>, greater<T>>;
+template<typename T>
+using max_heap = priority_queue<T, vector<T>, less<T>>;
+
+namespace Read {
+int Int() {
+  int x;
+  cin >> x;
+  return x;
+}
+int64 Int64() {
+  int64 x;
+  cin >> x;
+  return x;
+}
+char Char() {
+  char c;
+  cin >> c;
+  return c;
+}
+string String() {
+  string s;
+  cin >> s;
+  return s;
+}
+double Double() {
+  return stod(String());
+}
+ld LongDouble() {
+  return stold(String());
+}
+template<typename T1, typename T2>
+pair<T1, T2> Pair() {
+  pair<T1, T2> p;
+  cin >> p.first >> p.second;
+  return p;
+}
+template<typename T>
+vector<T> Vec(const int n) {
+  vector<T> v(n);
+  for (T &x : v) {
+    cin >> x;
+  }
+  return v;
+}
+template<typename T>
+vector<vector<T>> VecVec(const int n, const int m) {
+  vector<vector<T>> v(n);
+  for (vector<T> &vec : v) {
+    vec = Vec<T>(m);
+  }
+  return v;
+}
+}//namespace Read
+
+constexpr int kInf = 1e9 + 10;
+constexpr int64 kInf64 = 1e15 + 10;
+constexpr int kMod = 1e9 + 7;
+constexpr int kMaxN = 2e5 + 10;
+
+inline void solution(const auto &two) {
+  const int n = Read::Int();
+  const vector<int> a = Read::Vec<int>(n);
+
+  int ans = 0;
+  for (int i = 0; i < n; i++) {
+    const int two_count = two[n - 1] - two[i] - two[n - i - 1];
+    if (two_count == 0) ans ^= a[i];
+  }
+
+  cout << ans << '\n';
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  //freopen(".in", "r", stdin);
+  //freopen(".out", "w", stdout);
+
+  cout << fixed << setprecision(10);
+
+  vector<int> two(kMaxN);
+  for (int i = 2; i < kMaxN; i++) {
+    for (int pow2 = 2; pow2 <= i; pow2 += pow2) {
+      two[i] += i / pow2;
+    }
+  }
+
+  int testcases = 1;
+  //cin >> testcases;
+  while (testcases--) {
+    solution(two);
+  }
+}
